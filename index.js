3ae0f414a7bb4baa8dd08da8b4f8e99713ce61b8
caffeinated-tech <liam@caffeinated.tech>
2018-02-13 19:36:51 +0000

    clean up arduino code, added hall effect sensors and servo

--- index.js
+++ index.js
@@ -5,34 +5,38 @@ const express = require("express");
 const app = express();
 
 // load scripts for controlling RP GPIO & arduino
-const GPIO = require('./scripts/gpio.js');
-const Arduino = require('./scripts/arduino.js');
+// const GPIO = require('./scripts/gpio.js');
+// TODO: replace rpio with the ardion - lights will be controlled by the 
+//   arduino! Should make everything eaier to control.
+require('./scripts/arduino.js');
+// const arduino = new Arduino();
 
-// configure the callbacks for what happens when the brightness changes
-Arduino.onHighBrightness = function(){
-  GPIO.turnLightsOff();
-}
-Arduino.onLowBrightness = function(){
-  GPIO.turnLightsOn();
-}
+// // configure the callbacks for what happens when the brightness changes
+// arduino.onHighBrightness = function(){
+//   // console.log('on')
+//   // GPIO.turnLightsOff();
+// }
+// arduino.onLowBrightness = function(){
+//   // GPIO.turnLightsOn();
+//   // console.log('off')
+// }
 
-// Simple API for toggling the lights via get requests to urls:
-//  GET /lights/on : turn lights on
-//  GET /lights/off : turn lights off
-app.get("/lights/:value", function(req, res){
-	let value = req.params.value;
-	if(value == 'on'){
-    GPIO.turnLightsOn();
-		res.send("Lights have been turned on!");
-	} else if(value == 'off') {
-    GPIO.turnLightsOff();
-		res.send("Lights have been turned OFF!");
-	} else {
-		console.log("unknown command ", value);
-		res.send("Unknown command!\nDon't know how to turn lights " + value);
-	}
-});
-
-console.log("listening on port 3000")
-app.listen(3000);
+// // Simple API for toggling the lights via get requests to urls:
+// //  GET /lights/on : turn lights on
+// //  GET /lights/off : turn lights off
+// app.get("/lights/:value", function(req, res){
+// 	let value = req.params.value;
+// 	if(value == 'on'){
+//     GPIO.turnLightsOn();
+// 		res.send("Lights have been turned on!");
+// 	} else if(value == 'off') {
+//     GPIO.turnLightsOff();
+// 		res.send("Lights have been turned OFF!");
+// 	} else {
+// 		console.log("unknown command ", value);
+// 		res.send("Unknown command!\nDon't know how to turn lights " + value);
+// 	}
+// });
+// console.log("listening on port 3000")
+// app.listen(3000);
 
--- scripts/arduino.js
+++ scripts/arduino.js
@@ -4,106 +4,96 @@
 //   StandardFirmataPlus
 const five = require('johnny-five');
 
+///////////////////////////////////////////////////////////////////////////////
+// arduino
+///////////////////////////////////////////////////////////////////////////////
 
-class Arduino {
-  constructor(){
-    super();
-    // Connect to the arduino on the port it's connected to, and turn of the 
-    //   interactive console
-    this.board = five.Board({
-      port: '/dev/ttyUSB0',
-      repl: false
-    });
-    // use a low and high threshold to prevent flickering when the value is 
-    //  close to the threshold
-    this.threshold = {
-      low: 0.45,
-      high: 0.55
-    }
-    this.board.on('ready', function(){
-      // sample all reads twice per second
-      this.samplingInterval(500);
-      
-      this._configurePhotoResistor();
-    });
+// Connect to the arduino on the port it's connected to, and turn of the 
+//   interactive console
+const arduino = five.Board({
+  port: '/dev/ttyUSB0',
+  repl: false
+});
 
-  }
+///////////////////////////////////////////////////////////////////////////////
+// light sensor
+///////////////////////////////////////////////////////////////////////////////
 
-  // this is a placeholder callback which needs to be set to tell the arduino 
-  //   what to do when the brightness rises above the high threshold
-  onHighBrightness (){
-    return null;
-  }
-
-  // this is a placeholder callback which needs to be set to tell the arduino 
-  //   what to do when the brightness falls below the low threshold
-  onLowBrightness (){
-    return null;
-  }
-
-  // private methods prefixed with _
-
-  // initialize photoresistor and toggle lights on off when it falls below the 
-  //   threshold.
-  _configurePhotoResistor(){
-    // need a local reference to the current scope for the change listener
-    let arduino = this;
-    // photo resistor is connected on pin A0
-    this.photoResistor = new five.Light('A0');
-      photoResistor.on('change', function(){
-        if(this.level > arduino.threshold.high){
-          arduino.onHighBrightness();
-        } else if (this.level < arduino.threshold.log){
-          arduino.onLowBrightness();
-        }
-      });
+// use a low and high threshold to prevent flickering when the value is 
+//  close to the threshold
+const lightThreshold = {
+  low: 0.45,
+  high: 0.55
+}
+const onHighBrightness = function() {
+  console.log('brightness is high! Turn off lights');
+}
+const onLowBrightness = function() {
+  console.log('brightness is low! Turn on lights');
+}
+const lightSensorCallback = function() {
+  if(this.level > lightThreshold.high){
+    onHighBrightness();
+  } else if (this.level < lightThreshold.low){
+    onLowBrightness();
   }
 }
-// initialize connection to arduino
-var board = five.Board({
-	port: '/dev/ttyUSB0',
-	repl: false
+
+///////////////////////////////////////////////////////////////////////////////
+// Servo
+///////////////////////////////////////////////////////////////////////////////
+
+let moveBarrierDown = false;
+let servo = null
+const servoConfig = {
+  pin: 10,
+  startAt: 0,
+  range: [0,90]
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// Hall effect sensors
+///////////////////////////////////////////////////////////////////////////////
+
+const hallEffectThreshold = {
+  low: 500,
+  high: 520
+}
+const onHallEffectSensorTrigger = function(){
+  if(this.value > hallEffectThreshold.high || 
+     this.value < hallEffectThreshold.low){
+    console.log('a hall effect sensor was triggered');
+
+    // toggle the barrier (ie. servo)
+    moveBarrierDown = !moveBarrierDown
+    if(moveBarrierDown){
+      servo.to(90, 1000);
+    } else {
+      servo.to(0, 1000);
+    }
+    console.log('moveBarrierDown?', moveBarrierDown)
+  }
+}
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Initialize sensors
+///////////////////////////////////////////////////////////////////////////////
+
+arduino.on('ready', function(){
+  // sample all reads twice per second
+  this.samplingInterval(500);
+  // initialize LDR and toggle lights on off when brightness falls below the 
+  //   threshold.
+  new five.Light('A0').on('change', lightSensorCallback);
+  // initialize two hall effect sensors
+  new five.Sensor('A1').on('change', onHallEffectSensorTrigger);
+  new five.Sensor('A2').on('change', onHallEffectSensorTrigger);
+  servo = new five.Servo(servoConfig);
 });
 
-// threshold for when the leds should turn on / off
-// lights off, only ambeint lighting (monitors etc.) is around 0.18
-// lights on gives a reading around 0.75
-var threshold = 0.5;
-
-board.on('ready', function(){
-	// sample all reads twice per second
-	this.samplingInterval(500);
-	// initialize photoresistor amd toggle lights on off when it falls below the threshold.
-	var photoResistor = new five.Light('A0');
-	photoResistor.on('change', function(){
-		console.log("light level: " + this.level)
-		if(this.level > threshold){
-			toggleLights(rpio.LOW);
-		} else {
-			toggleLights(rpio.HIGH);
-		}
-	});
-});
-
-
-// API to toggle lights
-app.get("/lights/:value", function(req, res){
-	var value = req.params.value;
-
-	if(value == 'on'){
-		console.log("Toggle Lights ON");
-		toggleLights(rpio.HIGH)
-		res.send("Lights have been turned on!");
-	} else if(value == 'off') {
-		console.log("Toggle Lights OFF");
-		toggleLights(rpio.LOW)
-		res.send("Lights have been turned OFF!");
-	} else {
-		console.log("unknown command ", value);
-		res.send("Unknown command!\nDon't know how to turn lights " + value);
-	}
-});
-
-console.log("listening on port 3000")
-app.listen(3000);
-
+// TODO: will we be exporting anything???
+module.exports = null;
